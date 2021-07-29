91e4cf24712e9a6707fc9f162852ef3b52892e69
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-29 15:41:08 -0500

    Part 6, assigning 3.14 to myNumber and getting error incompatible types: possible lossy conversion from double to long

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
         String myString = "3.14159";
         System.out.println(myString);
 
-        long onePointTwentyOneGigaWatts;
-        System.out.println(onePointTwentyOneGigaWatts);
+        long myNumber = 3.14;
+        System.out.println(myNumber);
     }
 }
