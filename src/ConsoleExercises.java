e92a02666cc757e9d704c5c5b6627986652fe29f
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-02 10:07:52 -0500

    Prompting user for an integer and storing that value in an int variable. An InputMismatchException occurs if something other than an integer is entered

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
-        double pi = 3.14159;
+//        double pi = 3.14159;
+//
+//        System.out.format("The value of pi is approximately %.2f.%n", pi);
 
-        System.out.format("The value of pi is approximately %.2f.%n", pi);
+        // Prompt user to enter an integer and store that value in an int variable using the nextInt method.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int userInteger = scanner.nextInt();
+
+        System.out.format("You entered: %d%n", userInteger);
     }
 }
