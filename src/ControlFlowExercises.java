abf72776d86e5460d0cdc68f07ee259b49e1d092
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-02 16:51:03 -0500

    Refactoring to use for loop, 0 to 100 by 2s

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,19 +1,16 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        // b. Do While
-        // Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        // c. For
+        // Create a for loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
         // Alter your loop to count backwards by 5's from 100 to -10.
-        // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+        // Create a for loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
         // 2
         // 4
         // 16
         // 256
         // 65536
-        long number = 2L;
-
-        do {
-            System.out.println(number);
-            number *= number;
-        } while (number < 1000000);
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+        }
     }
 }
