5973551ea9611b72b798d5ffdf5716a995d1e725
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-02 16:23:42 -0500

    Completed 1b, outputing 0 to 100 by 2s using do while loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,19 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        // 1. loop basics
-        // a. While
-        // Create an integer variable i with a value of 5.
-        // Create a while loop that runs so long as i is less than or equal to 15
-        // Each loop iteration, output the current value of i, then increment i by one.
-        // Your output should look like this: 5 6 7 8 9 10 11 12 13 14 15
-        int i = 5;
+        // b. Do While
+        // Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        // Alter your loop to count backwards by 5's from 100 to -10.
+        // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+        // 2
+        // 4
+        // 16
+        // 256
+        // 65536
+        int evens = 0;
 
-        while (i <= 15) {
-            System.out.printf("%d ", i++);
-        }
+        do {
+            System.out.println(evens);
+            evens += 2;
+        } while (evens <= 100);
     }
 }
