56bd2f89899f877ceb6d337ea934d5d774999381
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-02 10:16:56 -0500

    Finished Explore the Scanner class, part 2. If less than 3 words are entered the system waits for another entry. If more than 3 words, the last is ignored

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -9,9 +9,19 @@ public class ConsoleExercises {
         // Prompt user to enter an integer and store that value in an int variable using the nextInt method.
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter an integer: ");
-        int userInteger = scanner.nextInt();
+//        System.out.print("Enter an integer: ");
+//        int userInteger = scanner.nextInt();
+//
+//        System.out.format("You entered: %d%n", userInteger);
 
-        System.out.format("You entered: %d%n", userInteger);
+        // Prompt user to enter 3 words and store each in a separate variable.
+        // Display them in the console, each on a newline.
+        System.out.print("Enter 3 words: ");
+
+        String userWordOne = scanner.next();
+        String userWordTwo = scanner.next();
+        String userWordThree = scanner.next();
+
+        System.out.format("%s%n%s%n%s%n", userWordOne, userWordTwo, userWordThree);
     }
 }
