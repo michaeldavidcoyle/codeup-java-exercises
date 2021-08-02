fe279e571f72d5f2757c4e1c00dae1b6b29ef294
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-02 18:50:47 -0500

    Completed part 3, displaying a table of powers from 1 to user input integer

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,20 +1,38 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        // 2. FizzBuzz
-        // Write a program that prints the numbers from 1 to 100.
-        // For multiples of three: print “Fizz” instead of the number.
-        // For the multiples of five: print “Buzz”.
-        // For numbers which are multiples of both three and five: print “FizzBuzz”.
-        for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+        // 3. Display a table of powers.
+        // Prompt the user to enter an integer.
+        // Display a table of squares and cubes from 1 to the value entered.
+        // Ask if the user wants to continue.
+        // Assume that the user will enter valid data.
+        // Only continue if the user agrees to.
+        // What number would you like to go up to? 5
+        // Here is your table!
+        //  number | squared | cubed
+        //  ------ | ------- | -----
+        //  1      | 1       | 1
+        //  2      | 4       | 8
+        //  3      | 9       | 27
+        //  4      | 16      | 64
+        //  5      | 25      | 125
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Table of Squares and Cubes");
+        System.out.print("Please enter maximum integer value: ");
+        int max = scanner.nextInt();
+
+        int square;
+        int cube;
+
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for (int i = 1; i <= max; i++) {
+            square = i * i;
+            cube = square * i;
+
+            System.out.printf("%-7d| %-7d | %-5d%n", i, square, cube);
         }
     }
 }
