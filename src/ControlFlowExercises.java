c790dcd3c7bcbe0eaf15af1a3c3673a6224f13b6
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-02 16:18:06 -0500

    Complete 1a, outputing 0 to 100 by 2s with while loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        // 1. loop basics
+        // a. While
+        // Create an integer variable i with a value of 5.
+        // Create a while loop that runs so long as i is less than or equal to 15
+        // Each loop iteration, output the current value of i, then increment i by one.
+        // Your output should look like this: 5 6 7 8 9 10 11 12 13 14 15
+        int i = 5;
+
+        while (i <= 15) {
+            System.out.printf("%d ", i++);
+        }
     }
 }
