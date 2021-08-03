758e024584c78d5017fe37968964be17f2634f24
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-03 15:53:15 -0500

    File set up for Java Methods exercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,17 @@
+public class MethodsExercises {
+    /*
+    * Create four separate methods. Each will perform an arithmetic operation:
+    * Addition
+    * Subtraction
+    * Multiplication
+    * Division
+    * Each function needs to take two parameters/arguments so that the operation can be performed.
+    * Test your methods and verify the output.
+    * Add a modulus method that finds the modulus of two numbers.
+    * Food for thought: What happens if we try to divide by zero? What should happen?
+    */
+
+    public static void main(String[] args) {
+
+    }
+}
