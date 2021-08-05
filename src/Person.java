083795ded12e28054323b3c15c78e1a992ca0574
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-05 15:28:28 -0500

    Successfully created constructor for Person class and instantiated new Person object

--- src/Person.java
+++ src/Person.java
@@ -1,14 +1,24 @@
 public class Person {
     private String name;
 
-    public String getName(){
-        // TODO: return the person's name
+    public Person(String name) {
+        this.name = name;
     }
 
-    public void setName(String name){
-        // TODO: change the name property to the passed value
-    }
-    public void sayHello(){
-        // TODO: print a message to the console using the person's name
+//    public String getName(){
+//        // TODO: return the person's name
+//    }
+//
+//    public void setName(String name){
+//        // TODO: change the name property to the passed value
+//    }
+//
+//    public void sayHello(){
+//        // TODO: print a message to the console using the person's name
+//    }
+
+    public static void main(String[] args) {
+        Person mdc = new Person("Michael");
+        System.out.println(mdc.name);
     }
 }
