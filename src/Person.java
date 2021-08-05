c1c742d69893a8a098c5d960fc94af2d39fc36f3
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-05 15:43:00 -0500

    Completed Understanding references section

--- src/Person.java
+++ src/Person.java
@@ -23,5 +23,23 @@ public class Person {
     public static void main(String[] args) {
         Person mdc = new Person("Michael");
         mdc.sayHello();
+
+        System.out.println("Understanding references: ");
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName())); // should be true because they have the same name
+//        System.out.println(person1 == person2); // false because they are different objects
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2); // wasn't sure but turn out to be true
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName()); // John
+        System.out.println(person2.getName()); // John
+        person2.setName("Jane");
+        System.out.println(person1.getName()); // Expecting John, but printed Jane, so changing one changes the other.
+        System.out.println(person2.getName()); // Expecting Jane, got Jane
     }
 }
