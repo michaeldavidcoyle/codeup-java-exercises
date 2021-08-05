0eb3f11534ff2cd20a182d89da600e0a999523c0
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-05 16:31:06 -0500

    The other getInt method with range params seems to be working

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -12,11 +12,18 @@ public class InputTest {
         boolean userWantChat = input.yesNo();
 
         if (userWantChat) {
-            System.out.println("Great. Let's talk!");
+            System.out.println("Great. Let's talk about numbers!");
 
             System.out.printf("%s, what it your favorite integer? ", userName);
             int userFavoriteInteger = input.getInt();
-            System.out.printf("Hmm, %d is a good one.", userFavoriteInteger);
+            System.out.printf("Hmm, %d is a good one.%n", userFavoriteInteger);
+
+            int min = userFavoriteInteger + 1;
+            int max = min + 10;
+
+            System.out.printf("If you had to pick a number from %d to %d, what would you choose? ", min, max);
+            int userRangeInteger = input.getInt(min, max);
+            System.out.printf("Oh, %d. Interesting.", userRangeInteger);
         } else {
             System.out.println("No problem, maybe some other time.");
         }
