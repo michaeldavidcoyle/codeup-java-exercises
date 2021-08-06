415a087a2ec980c0dfc111bfc619ab81b2af657c
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-06 15:57:24 -0500

    Overriding getArea and getPerimeter methods

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,22 @@
 package shapes;
 
 public class Square extends Rectangle {
+    private double side;
+
     public Square(double side) {
         super(side, side);
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+//        System.out.println("square"); // tells if method is called from Square or Rectangle
+        return Math.pow(side, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+//        System.out.println("square"); // tells if method is called from Square or Rectangle
+        return side * 4;
     }
 }
