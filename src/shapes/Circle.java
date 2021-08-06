6a5b5ad053e80cde6a7e37c64b71701e3146ec49
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-06 09:05:18 -0500

    Created getArea emthod for Circle class

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -6,4 +6,8 @@ public class Circle {
     public Circle(double radius) {
         this.radius = radius;
     }
+
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
 }
