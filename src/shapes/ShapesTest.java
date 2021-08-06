4d02c533d863747f15a1aff5407c6264ccdd6118
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-06 15:29:16 -0500

    Tests for getArea and getPerimeter both passing for new Square object

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -7,5 +7,9 @@ public class ShapesTest {
 
         System.out.printf("box1 area: %f", box1.getArea());
         System.out.printf("%nbox1 perimeter: %f", box1.getPerimeter());
+
+        Rectangle box2 = new Square(5);
+        System.out.printf("%nbox2 area: %f", box2.getArea());
+        System.out.printf("%nbox2 perimeter: %f", box2.getPerimeter());
     }
 }
