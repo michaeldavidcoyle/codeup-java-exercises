47ce1a7d211c4eff47419ff82f3f5c3a28c86ce0
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-06 15:56:34 -0500

    Moved new line chars to end of strings

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,11 +5,11 @@ public class ShapesTest {
         // Doesn't the curriculum discourage using digits in variable names?
         Rectangle box1 = new Rectangle(5, 4);
 
-        System.out.printf("box1 area: %f", box1.getArea());
-        System.out.printf("%nbox1 perimeter: %f", box1.getPerimeter());
+        System.out.printf("box1 area: %f%n", box1.getArea());
+        System.out.printf("box1 perimeter: %f%n", box1.getPerimeter());
 
         Rectangle box2 = new Square(5);
-        System.out.printf("%nbox2 area: %f", box2.getArea());
-        System.out.printf("%nbox2 perimeter: %f", box2.getPerimeter());
+        System.out.printf("box2 area: %f%n", box2.getArea());
+        System.out.printf("box2 perimeter: %f%n", box2.getPerimeter());
     }
 }
