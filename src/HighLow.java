02e547fb6a5e7ef5183d2a74849b30f3d5911f5b
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-07 13:57:48 -0500

    Set limit on number of guesses

--- src/HighLow.java
+++ src/HighLow.java
@@ -4,20 +4,29 @@ public class HighLow {
     public static void main(String[] args) {
         int myNumber = MethodsExercises.randomInteger(1, 100);
         int guessCount = 0;
+        int maxGuessCount = 10;
+        boolean gameIsLost = false;
         int guess;
 
         do {
             guess = MethodsExercises.getInteger(1, 100);
             guessCount++;
 
-            if (guess < myNumber) {
+            if (guessCount == maxGuessCount && guess != myNumber) {
+                gameIsLost = true;
+                break;
+            } else if (guess < myNumber) {
                 System.out.println("Higher");
             } else if (guess > myNumber) {
                 System.out.println("Lower");
             }
         } while (guess != myNumber);
 
-        System.out.println("Good guess!");
-        System.out.printf("My number was %d. You made %d guesses.%n", myNumber, guessCount);
+        if (gameIsLost) {
+            System.out.printf("Sorry, 10 guesses is the limit. My number was %d.%n", myNumber);
+        } else {
+            System.out.println("Good guess!");
+            System.out.printf("My number was %d. You made %d guesses.%n", myNumber, guessCount);
+        }
     }
 }
