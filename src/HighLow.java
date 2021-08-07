8f00b45b5cc1f5fa889aa59ae3fd466e7aa9d543
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-07 13:29:07 -0500

    High low basic functionality complete

--- src/HighLow.java
+++ src/HighLow.java
@@ -3,8 +3,18 @@ import java.lang.reflect.Method;
 public class HighLow {
     public static void main(String[] args) {
         int myNumber = MethodsExercises.randomInteger(1, 100);
-        int guess = MethodsExercises.getInteger(1, 100);
+        int guess;
 
-        System.out.printf("You guessed: %d", guess);
+        do {
+            guess = MethodsExercises.getInteger(1, 100);
+
+            if (guess < myNumber) {
+                System.out.println("Higher");
+            } else if (guess > myNumber) {
+                System.out.println("Lower");
+            }
+        } while (guess != myNumber);
+
+        System.out.printf("Good guess! My number was %d.%n", myNumber);
     }
 }
