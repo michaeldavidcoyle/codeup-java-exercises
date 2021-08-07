6c4960299d3613966aeba4b30ab17373f7cd631a
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-07 13:18:20 -0500

    Prompting user to guess number and saving in guess variable

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.Method;
+
 public class HighLow {
     public static void main(String[] args) {
         int myNumber = MethodsExercises.randomInteger(1, 100);
+        int guess = MethodsExercises.getInteger(1, 100);
 
-        System.out.printf("My number is %d%n", myNumber);
+        System.out.printf("You guessed: %d", guess);
     }
 }
