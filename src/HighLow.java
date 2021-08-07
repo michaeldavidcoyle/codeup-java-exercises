45353181f80ad202c6c1d4e3a3292f5954176bc3
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-07 13:12:05 -0500

    Picking a number from 1 to 100

---
+++ src/HighLow.java
@@ -0,0 +1,7 @@
+public class HighLow {
+    public static void main(String[] args) {
+        int myNumber = MethodsExercises.randomInteger(1, 100);
+
+        System.out.printf("My number is %d%n", myNumber);
+    }
+}
