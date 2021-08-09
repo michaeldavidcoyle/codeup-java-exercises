7646765ec7ccca609ec182a9fd1d47f210b3c870
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-09 10:35:21 -0500

    Refactored to user super.length in place of having to create side variable

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,22 +1,19 @@
 package shapes;
 
 public class Square extends Rectangle {
-    protected double side;
-
     public Square(double side) {
         super(side, side);
-        this.side = side;
     }
 
     @Override
     public double getArea() {
 //        System.out.println("square"); // tells if method is called from Square or Rectangle
-        return Math.pow(side, 2);
+        return Math.pow(super.length, 2);
     }
 
     @Override
     public double getPerimeter() {
 //        System.out.println("square"); // tells if method is called from Square or Rectangle
-        return side * 4;
+        return super.length * 4;
     }
 }
