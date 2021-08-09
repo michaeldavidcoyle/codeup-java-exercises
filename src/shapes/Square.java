5ea5e382bb817731c89565250b3cf90abab07dec
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-09 15:52:22 -0500

    Part 1 of Abstract and Interfaces Exercises: Remove the code from the body of your Rectangle and Square classes.

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,19 +1,5 @@
 package shapes;
 
 public class Square extends Rectangle {
-    public Square(double side) {
-        super(side, side);
-    }
 
-    @Override
-    public double getArea() {
-//        System.out.println("square"); // tells if method is called from Square or Rectangle
-        return Math.pow(super.length, 2);
-    }
-
-    @Override
-    public double getPerimeter() {
-//        System.out.println("square"); // tells if method is called from Square or Rectangle
-        return super.length * 4;
-    }
 }
