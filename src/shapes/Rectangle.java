7a9f41607bd00d259a1cc0e6cde7772392fdd654
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-09 16:27:58 -0500

    Updated Rectangle to extend Quadrilateral and implement Measurable

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,5 +1,18 @@
 package shapes;
 
-public class Rectangle {
+public class Rectangle extends Quadrilateral implements Measurable{
 
+    public Rectangle(double length, double width) {
+        super(length, width);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (length * 2) + (width * 2);
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
 }
