dd858d679648e1d950f735eb163fbe754299ebe6
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-10 12:19:31 -0500

    Printing hyphenated server name, adjective-noun combination, to console

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,16 +1,21 @@
 public class ServerNameGenerator {
 
-    protected String[] adjectives = {
+    protected static String[] adjectives = {
             "flat", "round", "silly", "serious", "robust", "tired", "energetic", "viscous", "soupy", "arrogant",
             "humble", "blue", "sleepy", "average", "slow", "quick", "dangerous", "adorable", "courageous", "defiant"
     };
 
-    protected String[] nouns = {
+    protected static String[] nouns = {
             "boat", "desk", "phone", "camp", "bottle", "toy", "circle", "vector", "key", "horse",
             "beast", "hand", "collar", "pipe", "goat", "knight", "board", "net", "number", "wingnut"
     };
 
     public static String getRandomString(String[] stringsArray) {
         return stringsArray[ MethodsExercises.randomInteger(0, stringsArray.length - 1) ];
+    }
+
+    public static void main(String[] args) {
+        String serverName = getRandomString(adjectives) + '-' + getRandomString(nouns);
+        System.out.println(serverName);
     }
 }
