809dc1842245abaad55dd0bb20d764493246d71a
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-10 12:11:46 -0500

    Created ServerNameGenerator class with arrays of nouns and adjectives

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,12 @@
+public class ServerNameGenerator {
+
+    protected String[] adjectives = {
+            "flat", "round", "silly", "serious", "robust", "tired", "energetic", "viscous", "soupy", "arrogant",
+            "humble", "blue", "sleepy", "average", "slow", "quick", "dangerous", "adorable", "courageous", "defiant"
+    };
+
+    protected String[] nouns = {
+            "boat", "desk", "phone", "camp", "bottle", "toy", "circle", "vector", "key", "horse",
+            "beast", "hand", "collar", "pipe", "goat", "knight", "board", "net", "number", "wingnut"
+    };
+}
