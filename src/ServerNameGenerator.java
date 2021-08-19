87c34dcb34ae0f3f39c4b9ee38ea01def39ddf58
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-18 20:53:17 -0500

    Updated ServerNameGenerator to read from file

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,88 +1,91 @@
-//import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class ServerNameGenerator {
-//
-//    protected static String[] adjectives = {
-//            "flat", "round", "silly", "serious", "robust", "tired", "energetic", "viscous", "soupy", "arrogant",
-//            "humble", "blue", "sleepy", "average", "slow", "quick", "dangerous", "adorable", "courageous", "defiant"
-//    };
-//
-//    protected static String[] nouns = {
-//            "boat", "desk", "phone", "camp", "bottle", "toy", "circle", "vector", "key", "horse",
-//            "beast", "hand", "collar", "pipe", "goat", "knight", "board", "net", "number", "wingnut"
-//    };
-//
-//    public static String getRandomString(String[] stringsArray) {
-//        return stringsArray[ (int) (Math.random() * stringsArray.length) ];
-//    }
-//
-//    public static Path generateAdjectivesFile() throws IOException {
-//        String directory = "data";
-//        String adjectives = "adjectives.txt";
-//
-//        Path dataDirectory = Paths.get(directory);
-//        Path adjectivesFile = Paths.get(directory, adjectives);
-//
-//        if (Files.notExists(dataDirectory)) {
-//            Files.createDirectories(dataDirectory);
-//        }
-//
-//        if (Files.notExists(adjectivesFile)) {
-//            Files.createFile(adjectivesFile);
-//        }
-//
-//        return adjectivesFile;
-//    }
-//
-//    public static Path generateNounsFile() throws IOException {
-//        String directory = "data";
-//        String nouns = "nouns.txt";
-//
-//        Path dataDirectory = Paths.get(directory);
-//        Path nounsFile = Paths.get(directory, nouns);
-//
-//        if (Files.notExists(dataDirectory)) {
-//            Files.createDirectories(dataDirectory);
-//        }
-//
-//        if (Files.notExists(nounsFile)) {
-//            Files.createFile(nounsFile);
-//        }
-//
-//        return nounsFile;
-//    }
-//
-//    public static ArrayList<String> getAdjectives() {
-//        Path adjectivesFile;
-//        List<String> adjectivesList;
-//        try {
-//            adjectivesFile = generateAdjectivesFile();
-//            adjectivesList = Files.readAllLines(adjectivesFile);
-//        } catch (IOException e) {
-//            System.out.printf("Oops, something happened: %s%n", e.getMessage());
-//        }
-//
-//        List<String> lines = Arrays.asList("testing: 1, 5, 3", "testing: 2, 7, 4"); // create new List of strings
-//        Files.write(contactFile, lines);
-//    }
-//
-//    public static void main(String[] args) {
-////        String serverName = getRandomString(adjectives) + '-' + getRandomString(nouns);
-////        System.out.println(serverName);
-//
-//        try {
-//            generateAdjectivesFile();
-//            generateNounsFile();
-//        } catch (IOException e) {
-//            System.out.printf("Oops, something happened: %s%n", e.getMessage());
-//        }
-//
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class ServerNameGenerator {
+
+    private static String directory = "data";
+    private static Path dataDirectory = Paths.get(directory);
+
+    private static String adjectives = "adjectives.txt";
+    private static Path adjectivesFile = Paths.get(directory, adjectives);
+
+    private static String nouns = "nouns.txt";
+    private static Path nounsFile = Paths.get(directory, nouns);
+
+    private static List<String> altAdjectives = Arrays.asList(
+            "flat", "round", "silly", "serious", "robust", "tired", "energetic", "viscous", "soupy", "arrogant",
+            "humble", "blue", "sleepy", "average", "slow", "quick", "dangerous", "adorable", "courageous", "defiant"
+    );
+
+    private static List<String> altNouns = Arrays.asList(
+            "boat", "desk", "phone", "camp", "bottle", "toy", "circle", "vector", "key", "horse",
+            "beast", "hand", "collar", "pipe", "goat", "knight", "board", "net", "number", "wingnut"
+    );
+
+    public static String getRandomString(List<String> stringsList) {
+        return stringsList.get( (int) (Math.random() * stringsList.size()) );
+    }
+
+    public static Path generateAdjectivesFile() throws IOException {
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (Files.notExists(adjectivesFile)) {
+            Files.createFile(adjectivesFile);
+        }
+
+        return adjectivesFile;
+    }
+
+    public static Path generateNounsFile() throws IOException {
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (Files.notExists(nounsFile)) {
+            Files.createFile(nounsFile);
+        }
+
+        return nounsFile;
+    }
+
+    public static List<String> getAdjectives() {
+        Path adjectivesFile;
+
+        try {
+            adjectivesFile = generateAdjectivesFile();
+            return Files.readAllLines(adjectivesFile);
+        } catch (IOException e) {
+            System.out.printf("Oops, something happened: %s%n", e.getMessage());
+            return altAdjectives;
+        }
+    }
+
+    public static List<String> getNouns() {
+        Path nounsFile;
+
+        try {
+            nounsFile = generateNounsFile();
+            return Files.readAllLines(nounsFile);
+        } catch (IOException e) {
+            System.out.printf("Oops, something happened: %s%n", e.getMessage());
+            return altNouns;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        List<String> adjectivesList = getAdjectives();
+        List<String> nounsList = getNouns();
+
+        String serverName = getRandomString(adjectivesList) + '-' + getRandomString(nounsList);
+        System.out.println(serverName);
+
 //        System.out.println("Success.");
-//    }
-//}
+    }
+}
