86c4b0175f1d77dacc2c62ceed4ada92fdd183a3
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-26 11:56:31 -0500

    Set up files for pizza project.

---
+++ src/pizza/Pizza.java
@@ -0,0 +1,15 @@
+package pizza;
+
+/*
+ * Make a Pizza class. It should have fields like pizza type (e.g., Cheese, Pepperoni, Supreme, etc.),
+   size (Small, Medium, Large, and Extra Large), and crust type (Thin, Hand-tossed, Pan).
+ * Include accessors and an appropriate constructor.
+ * Make a PizzaShop class. This class will have your main() method for the program.
+ * Write a static method in the PizzaShop class to make some pizza. The method should return an ArrayList of Pizza.
+   Have the method store a few different types of pizza in the array list before returning it.
+ * Next, write a printPizza method that takes an ArrayList of Pizza and prints out the pizzas in the ArrayList.
+ * In your main, call makePizza and then call printPizza
+ */
+
+public class Pizza {
+}
