18d4d13f439316b79adf08c808c1fd0351fbbd5a
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-27 19:44:04 -0500

    Started building PizzaShop class, with fields for pizzas, crusts, sizes, meats and veggies.

---
+++ src/pizza/PizzaShop.java
@@ -0,0 +1,19 @@
+package pizza;
+
+import java.util.ArrayList;
+import util.Input;
+
+public class PizzaShop {
+
+    private static ArrayList<Pizza> pizzas;
+    private static String[] crusts = {"thin", "hand-tossed", "pan"};
+    private static String[] sizes = {"small", "medium", "large", "extra large"};
+    private static String[] meats = {
+            "pepperoni", "italian sausage", "canadian bacon", "bacon", "ground beef", "anchovies"
+    };
+    private static String[] veggies = {
+            "mushrooms", "black olives", "bell peppers", "onions", "tomatoes", "jalepeños"
+    };
+    private static Input input = new Input();
+
+}
