e4c28af077e2ee61a148a8dc979056f6ecef98e5
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-28 22:03:27 -0500

    Added method to test if number is prime.

--- src/util/MathHelpers.java
+++ src/util/MathHelpers.java
@@ -27,11 +27,34 @@ public class MathHelpers {
         return factors;
     }
 
+    public static boolean isPrime(int integer) {
+        if (integer <= 1) return false;
+        if (integer == 2) return true;
+        if (integer % 2 == 0) return false;
+
+        final double SQRT = Math.sqrt((double) integer);
+
+        for (int i = 3; i <= SQRT; i += 2) {
+            if (integer % i == 0) return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        int integer = input.getInt("Enter an integer to factorize: ");
+//        int integer = input.getInt("Enter an integer to factorize: ");
+//
+//        ArrayList<Integer> factors = primeFactors(integer);
+//
+//        System.out.printf("The prime factors of %d: %s", integer, factors.toString());
 
-        ArrayList<Integer> factors = primeFactors(integer);
+        int primeCandidate = input.getInt("Enter integer to test if prime: ");
 
-        System.out.printf("The prime factors of %d: %s", integer, factors.toString());
+        if (isPrime(primeCandidate)) {
+            System.out.printf("%d is prime.", primeCandidate);
+        } else {
+            ArrayList<Integer> factors = primeFactors(primeCandidate);
+            System.out.printf("%d is composite. Its prime factors are %s", primeCandidate, factors.toString());
+        }
     }
 }
