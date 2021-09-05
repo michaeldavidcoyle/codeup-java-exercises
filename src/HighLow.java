1acd7e641326e518e91a6c54497b90dcb8be80a3
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-05 00:00:30 -0500

    Changed prompts to use Input class and did away with max guesses for now.

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,7 +1,7 @@
 import util.Input;
 
 public class HighLow {
-    public static Input input = new Input;
+    public static Input input = new Input();
 
     public static void giveHint(int guess, int actual) {
         if (guess < actual) {
@@ -16,30 +16,35 @@ public class HighLow {
     }
 
     public static void main(String[] args) {
-        int myNumber = randomInteger(1, 100);
+        int min = 1;
+        int max = input.getInt("Enter a maximum for the High/Low Game: ");
+
+        int myNumber = randomInteger(min, max);
+
         int guessCount = 0;
-        int maxGuessCount = 10;
-        boolean gameIsLost = false;
+//        int maxGuessCount = 10;
+
+//        boolean gameIsLost = false;
         int guess;
 
         do {
-            guess = MethodsExercises.getInteger(1, 100);
+            guess = input.getInt("Guess my number: ", min, max);
             guessCount++;
 
-            if (guessCount == maxGuessCount && guess != myNumber) {
-                gameIsLost = true;
-                break;
-            }
+//            if (guessCount == maxGuessCount && guess != myNumber) {
+//                gameIsLost = true;
+//                break;
+//            }
 
             giveHint(guess, myNumber);
 
         } while (guess != myNumber);
 
-        if (gameIsLost) {
-            System.out.printf("Sorry, 10 guesses is the limit. My number was %d.%n", myNumber);
-        } else {
+//        if (gameIsLost) {
+//            System.out.printf("Sorry, 10 guesses is the limit. My number was %d.%n", myNumber);
+//        } else {
             System.out.println("Good guess!");
             System.out.printf("My number was %d. You made %d guesses.%n", myNumber, guessCount);
-        }
+//        }
     }
 }
